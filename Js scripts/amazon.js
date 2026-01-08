9633644a655bc59e12100ab5d4ccb636a9491f50
Sam00-B <saumikbarua0@gmail.com>
2026-01-07 20:39:50 -0600

    cart is added to the page and the add to cart button is working. The cart is an array of objects where each object has a productId, productName and quantity. When the add to cart button is clicked, it checks if the product is already in the cart. If it is, it increments the quantity. If it is not, it adds a new object to the cart with quantity 1. The cart is logged to the console after each update.

--- Js scripts/amazon.js
+++ Js scripts/amazon.js
@@ -46,11 +46,31 @@ products.forEach((product)=>{
         Added
     </div>
 
-    <button class="add-to-cart-button button-primary">
+    <button class="add-to-cart-button button-primary js-add-to-cart-button" 
+    data-product-id="${product.id}">
         Add to Cart
     </button>
     </div>
     `
 })
-console.log(productsHtml)
-document.querySelector('.js-products-grids').innerHTML=productsHtml
+document.querySelector('.js-products-grids').innerHTML=productsHtml;
+document.querySelectorAll('.js-add-to-cart-button').forEach((button)=>{
+    button.addEventListener('click',()=>{
+        const productId=button.dataset.productId;
+        let matchingItem;
+        cart.forEach((item)=>{
+            if (item.productId===productId){
+                matchingItem=item;
+            }
+        })
+        if (matchingItem){
+            matchingItem.quantity+=1;
+        } else{
+            cart.push({
+            productId: productId,
+            quantity:1
+        })
+        console.log(cart)
+    }
+    })
+})

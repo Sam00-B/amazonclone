c2cc72bfde319638f4cb89cd1be8d5f58a5e47a2
Sam00-B <saumikbarua0@gmail.com>
2026-01-09 22:36:00 -0600

    import {cart,removeFromCart} from "../data/cart.js"; import { products } from "../data/products.js"; import {formatCurrencency} from "./utlis/money.js"; make the delete button work by adding an event listener to each delete link. When the delete link is clicked, it should call the removeFromCart function with the appropriate product ID and then remove the corresponding cart item from the DOM.

--- Js scripts/checkout.js
+++ Js scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from "../data/cart.js";
+import {cart,removeFromCart} from "../data/cart.js";
 import { products } from "../data/products.js";
 import {formatCurrencency} from "./utlis/money.js";
 let cartItemHtml="";
@@ -12,7 +12,7 @@ cart.forEach((item)=>{
     })
     cartItemHtml+=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -35,7 +35,7 @@ cart.forEach((item)=>{
                   <span class="update-quantity-link link-primary">
                     Update
                   </span>
-                  <span class="delete-quantity-link link-primary">
+                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id=${matchingProduct.id}>
                     Delete
                   </span>
                 </div>
@@ -90,3 +90,12 @@ cart.forEach((item)=>{
     `
 });
 document.querySelector(".js-order-summary").innerHTML=cartItemHtml;
+document.querySelectorAll(".js-delete-link").forEach((link)=>{
+    link.addEventListener("click",()=>{
+        let productId=link.dataset.productId;
+
+        removeFromCart(productId);
+        document.querySelector(`.js-cart-item-container-${productId}`).remove();
+
+    })
+})

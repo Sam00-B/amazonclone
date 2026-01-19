c61fb5e46f73cd364e2ec7eb1234d63dfe054a9f
Sam00-B <saumikbarua0@gmail.com>
2026-01-18 20:55:33 -0600

    implement dynamic payment summary rendering with formatted currency display

--- Js scripts/checkout/paymentSummary.js
+++ Js scripts/checkout/paymentSummary.js
@@ -1,6 +1,8 @@
 import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js"; 
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
+import { formatCurrencency } from "../utlis/money.js";
+
 export function RenderPaymentSummary(){
     let productPriceCents=0;
     let shippingPriceCents=0;
@@ -8,7 +10,7 @@ export function RenderPaymentSummary(){
         const product=getProduct(item.productId);
         productPriceCents+=product.priceCents*item.quantity;
         const deliveryOption=getDeliveryOption(item.deliveryOptionId);
-        shippingPriceCents+=deliveryOption.priceCents*item.quantity;
+        shippingPriceCents+=deliveryOption.priceCents;
         
 
         
@@ -17,6 +19,43 @@ export function RenderPaymentSummary(){
     let totalBeforeTaxCents=productPriceCents+shippingPriceCents;
     let taxCents=totalBeforeTaxCents*0.1;
     let totalCents=totalBeforeTaxCents+taxCents;
+    const paymentSummaryHtml=`
+    <div class="payment-summary-title">
+            Order Summary
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Items (3):</div>
+            <div class="payment-summary-money">${formatCurrencency(productPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Shipping &amp; handling:</div>
+            <div class="payment-summary-money">${formatCurrencency(shippingPriceCents)}</div>
+          </div>
+
+          <div class="payment-summary-row subtotal-row">
+            <div>Total before tax:</div>
+            <div class="payment-summary-money">${formatCurrencency(totalBeforeTaxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row">
+            <div>Estimated tax (10%):</div>
+            <div class="payment-summary-money">${formatCurrencency(taxCents)}</div>
+          </div>
+
+          <div class="payment-summary-row total-row">
+            <div>Order total:</div>
+            <div class="payment-summary-money">${formatCurrencency(totalCents)}</div>
+          </div>
+
+          <button class="place-order-button button-primary">
+            Place your order
+          </button>
+
+    `
+    document.querySelector('.js-payment-summary').innerHTML=paymentSummaryHtml
+
     
     
 

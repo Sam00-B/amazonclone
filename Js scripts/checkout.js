6beddd5cd0b9fe7df2848b5c901102f44cf2bcc1
Sam00-B <saumikbarua0@gmail.com>
2026-01-28 21:24:56 -0600

    created oop version of cart

--- Js scripts/checkout.js
+++ Js scripts/checkout.js
@@ -1,5 +1,6 @@
 import { RenderOrderSummary } from "./checkout/orderSummary.js";
 import { RenderPaymentSummary } from "./checkout/paymentSummary.js";
+import'../data/cart-oop.js'
 
 RenderOrderSummary();
 RenderPaymentSummary()

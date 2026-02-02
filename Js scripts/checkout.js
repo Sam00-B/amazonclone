1b7848647d64277652be6cbfdc6f1afd4cbf7884
Sam00-B <saumikbarua0@gmail.com>
2026-02-01 20:30:42 -0600

    Comment out unused import statement for cart-class in checkout.js

--- Js scripts/checkout.js
+++ Js scripts/checkout.js
@@ -1,6 +1,6 @@
 import { RenderOrderSummary } from "./checkout/orderSummary.js";
 import { RenderPaymentSummary } from "./checkout/paymentSummary.js";
-import'../data/cart-class.js';
+//import'../data/cart-class.js';
 
 RenderOrderSummary();
 RenderPaymentSummary()

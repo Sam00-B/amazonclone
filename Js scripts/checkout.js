442c094077a717139af5f3d3b93d84af5ae90ba6
Sam00-B <saumikbarua0@gmail.com>
2026-02-15 19:57:24 -0600

    Refactor product loading and rendering logic; implement loadProducts function for dynamic product retrieval

--- Js scripts/checkout.js
+++ Js scripts/checkout.js
@@ -1,6 +1,10 @@
 import { RenderOrderSummary } from "./checkout/orderSummary.js";
 import { RenderPaymentSummary } from "./checkout/paymentSummary.js";
-//import'../data/cart-class.js';
+import { loadProducts } from "../data/products.js";
+loadProducts(()=>{
 
-RenderOrderSummary();
-RenderPaymentSummary()
+    RenderOrderSummary();
+    RenderPaymentSummary()
+})
+
+
--- tests/data/cartTest.js
+++ tests/data/cartTest.js
@@ -28,6 +28,7 @@ describe("Test Suite: addToCart",()=>{
             return JSON.stringify([])
         })
         loadFromStorage();
+        localStorage.setItem.calls.reset();
     
         addToCart('e43638ce-6aa0-4b85-b27f-e1d07eb678c6')
         expect(cart.length).toEqual(1);

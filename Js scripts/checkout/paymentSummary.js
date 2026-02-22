b690edc727113e37fa7610d2f18c74291f1d229b
Sam00-B <saumikbarua0@gmail.com>
2026-02-22 12:40:40 -0600

    Add order handling in payment summary and implement order storage

--- Js scripts/checkout/paymentSummary.js
+++ Js scripts/checkout/paymentSummary.js
@@ -2,7 +2,7 @@ import { cart } from "../../data/cart.js";
 import { getProduct } from "../../data/products.js"; 
 import { getDeliveryOption } from "../../data/deliveryOptions.js";
 import { formatCurrencency } from "../utlis/money.js";
-
+import { addOrder } from "../../data/orders.js";
 export function RenderPaymentSummary(){
     let productPriceCents=0;
     let shippingPriceCents=0;
@@ -49,12 +49,32 @@ export function RenderPaymentSummary(){
             <div class="payment-summary-money">${formatCurrencency(totalCents)}</div>
           </div>
 
-          <button class="place-order-button button-primary">
+          <button class="place-order-button button-primary js-place-order">
             Place your order
           </button>
 
     `
     document.querySelector('.js-payment-summary').innerHTML=paymentSummaryHtml
+    document.querySelector('.js-place-order').addEventListener("click",async()=>{
+      try{
+        const response= await fetch("https://supersimplebackend.dev/orders",{
+        method:"POST",
+        headers:{
+          "Content-Type":"application/json"
+        },
+        body:JSON.stringify({
+          cart:cart
+        })
+      });
+      const order=await response.json();
+      addOrder(order);
+
+      }catch(error){
+        console.log("unexpected error .Please try again later")
+      }
+      window.location.href="orders.html"
+     
+    })
 
     
     

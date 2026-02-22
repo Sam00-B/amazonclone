c2543b6bcaf0fe0a46e1826884e789a71ab4d059
Sam00-B <saumikbarua0@gmail.com>
2026-02-21 23:53:20 -0600

    Refactor loadProducts invocation to use Promise for better asynchronous handling

--- Js scripts/checkout.js
+++ Js scripts/checkout.js
@@ -1,10 +1,20 @@
 import { RenderOrderSummary } from "./checkout/orderSummary.js";
 import { RenderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
-loadProducts(()=>{
+new Promise((resolve)=>{
+    loadProducts(()=>{
+        resolve();
+    })
 
+}).then(()=>{
     RenderOrderSummary();
     RenderPaymentSummary()
+
 })
+// loadProducts(()=>{
+
+//     RenderOrderSummary();
+//     RenderPaymentSummary()
+// })
 
 

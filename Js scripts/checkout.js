7f031b260d1e6dbfb6aad091348697797a6f85c1
Sam00-B <saumikbarua0@gmail.com>
2026-02-22 11:55:21 -0600

    Refactor loadPage function to use async/await for improved readability and maintainability

--- Js scripts/checkout.js
+++ Js scripts/checkout.js
@@ -2,20 +2,32 @@ import { RenderOrderSummary } from "./checkout/orderSummary.js";
 import { RenderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts,loadProductsFetch } from "../data/products.js";
 
-Promise.all([
-    loadProductsFetch(),
-    new Promise((resolve)=>{
-    loadProducts(()=>{
-        resolve();
-    })
+async function loadPage(){
+    await loadProductsFetch();
+        await new Promise((resolve)=>{
+        loadProducts(()=>{
+            resolve();
+        })
 
-}).then(()=>{
+    })
     RenderOrderSummary();
     RenderPaymentSummary()
+}
+loadPage();
+// Promise.all([
+//     loadProductsFetch(),
+//     new Promise((resolve)=>{
+//     loadProducts(()=>{
+//         resolve();
+//     })
 
-})
+// }).then(()=>{
+//     RenderOrderSummary();
+//     RenderPaymentSummary()
 
-])
+// })
+
+// ])
 // new Promise((resolve)=>{
 //     loadProducts(()=>{
 //         resolve();
